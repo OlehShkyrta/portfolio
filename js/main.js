e0c5b3dbd28ac7dab6c2818610f56992d0aea90d
Oleh Shkyrta <olegshkirta95@gmail.com>
2022-02-20 19:24:51 +0200

    20.02.2022_p1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,52 @@
-"use strict";
-
-// Burger button
-const menuBtn = document.querySelector('.menu__btn'),
-      mobileMenu = document.querySelector('.mobile__menu');
-
-menuBtn.addEventListener('click', e => {
-    e.preventDefault();
-    menuBtn.classList.toggle('menu__btn--active');
-    mobileMenu.classList.toggle('mobile__menu--active');
+$(document).ready(function() {
+    // Sticky heaxder
+    $('.header__menu-link').click(function(e) {
+        var linkHref = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top - 100
+        }, 1000);
+        e.preventDefault();
+    });
 });
 
-ScrollReveal().reveal('.about__inner', {duration: 4000, distance: '150px'});
-ScrollReveal().reveal('.contact', {duration: 4000, distance: '150px'});
+
+
+// Scroll animation
+ScrollReveal().reveal('.about__inner', {duration: 2000, distance: '150px'});
+ScrollReveal().reveal('.contact', {duration: 1000, distance: '200px'});
+
+
+//Highlighting the desired menu item when scrolling
+// var sections = $('section'),    
+//     header = $('header'),
+//     nav = $('.header__top-inner'),
+//     navHeight = nav.outerHeight(),
+//     menuLink = $('.header__menu');
+
+// $(window).on('scroll', function() {
+//     var currentPosition = $(this).scrollTop();
+
+//     sections.each(function() {
+//         var top = $(this).offset().top - navHeight,
+//             bottom = top + $(this).outerHeight(); 
+    
+//         if (currentPosition >= top && currentPosition <= bottom) {
+//             menuLink.find('a').removeClass('link-active');
+//             sections.removeClass('header__menu-link--active');
+
+//             $(this).addClass('link-active');
+//             menuLink.find('a[href="#'+$(this).attr('id')+'"]').addClass('link-active'); 
+//         }
+//     });
+// });
+
+// menuLink.find('a').on('click', function() {
+//     var $el = $(this),
+//         id = $el.attr('href');
+
+//     $('html, body').animate({
+//         scrollTop: $(id).offset().top - navHeight
+//     }, 500);
+
+//     return false;
+// });
